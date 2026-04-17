a47b65635e6febd1a7b7263ae6cb08d061d0677f
Abdalrahmaan Mohammad Alsayed <65967579+Abdo-404@users.noreply.github.com>
2026-04-17 03:47:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("active");
+}
